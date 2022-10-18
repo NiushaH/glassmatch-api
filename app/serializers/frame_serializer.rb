76f04790ba5f4bab58719d8fd0c0c94272606d2e
NiushaH <35556570+NiushaH@users.noreply.github.com>
2022-10-18 00:32:34 -0600

    removed options include gem code

--- app/serializers/frame_serializer.rb
+++ app/serializers/frame_serializer.rb
@@ -2,11 +2,7 @@ class FrameSerializer
   include FastJsonapi::ObjectSerializer
   has_many :ratings, serializer: RatingSerializer
   attributes :name, :style, :condition, :image_url, :OD_SPH, :OD_CYL, :OD_AXIS, :OD_PRISM, :OD_BASE, :OD_ADD, :OS_SPHERE, :OS_CYL, :OS_AXIS, :OS_PRISM, :OS_BASE, :OS_ADD
-  # options = {}
-  # options[:include] = [:ratings, :'ratings.score', :'ratings.comments', :'ratings.frame_id']
-  # options = { include: [:ratings] }
   attribute :ratings do |obj|
     RatingSerializer.new(obj.ratings)
   end
-
 end
